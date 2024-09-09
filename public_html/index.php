b2b22825155dee59e0aaeeb9f23061d6c3ec463d
Dylan Bui <duc@propzy.com>
2024-09-09 23:16:37 +0700

    update project

--- public_html/index.php
+++ public_html/index.php
@@ -17,19 +17,19 @@
 // 	define ('__SITE_URL', str_replace('/'.basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']));
 
 	// __BASE_URL : /adv_mvc/
- 	define ('__BASE_URL', __SITE_URL);
- 	// Co thu muc public_html 	
- 	define ('__PUBLIC_HTML', __SITE_URL);
- 	
- 	// ---- Khong Thay Doi ---- // 	
+	define ('__BASE_URL', __SITE_URL);
+ 	// Co thu muc public_html
+	define ('__PUBLIC_HTML', __SITE_URL);
+
+ 	// ---- Khong Thay Doi ---- //
  	define ('__ASSET_URL', __PUBLIC_HTML.'assets/');
  	define ('__IMAGE_URL', __ASSET_URL.'images/');
  	define ('__CSS_URL', __ASSET_URL.'css/');
  	define ('__JS_URL', __ASSET_URL.'js/');
  	
-	// the application directory path 
+	// the application directory path
 	define ('__APP_PATH', __SITE_PATH.'/app');
-	define ('__VIEW_PATH', __APP_PATH.'/views');	
+	define ('__VIEW_PATH', __APP_PATH.'/views');
 	define ('__LAYOUT_PATH', __APP_PATH.'/layouts');
 	// define ('__HELPER_PATH', __APP_PATH.'/helpers');
 //	define ('__CONFIG_PATH', __APP_PATH.'/config');
@@ -40,16 +40,16 @@
 	define ('__DATA_PATH', __SITE_PATH . '/public_html/data/');
 	define ('__DATA_URL', __PUBLIC_HTML . 'data/');
 
-	define ('__CONTROLLER_NAMESPACE', 'App\Controller');	
+	define ('__CONTROLLER_NAMESPACE', 'App\Controller');
 
  //    // Load config files. Global config file
 	// require __SITE_PATH.'/app/libraries/Core/Psr4Autoloader.php';
 	// // instantiate the loader
  // 	$loader = new Psr4Autoloader();
  // 	// register the base directories for the namespace prefix
- 	
- // 	$loader->addNamespace(__CONTROLLER_NAMESPACE, __APP_PATH.'/controllers'); 	
- // 	$loader->addNamespace('App\Lib', __SITE_PATH.'/app/libraries'); 	
+
+ // 	$loader->addNamespace(__CONTROLLER_NAMESPACE, __APP_PATH.'/controllers');
+ // 	$loader->addNamespace('App\Lib', __SITE_PATH.'/app/libraries');
  // 	$loader->addNamespace('App\Model', __SITE_PATH.'/app/models');
  // 	$loader->addNamespace('App\Helper', __SITE_PATH.'/app/helpers');
  // 	// register the autoloader
@@ -82,32 +82,32 @@
 	$registry = null;
 	$config = null;
 	
-	require __SITE_PATH . '/admin/startup.php';	
+	require __SITE_PATH . '/admin/startup.php';
 
 	$oBenchmark = new \App\Lib\Core\Benchmark();
-	$oBenchmark->mark('code_start');			
+	$oBenchmark->mark('code_start');
 
  	/*** a new registry object ***/
- 	$registry = new \App\Lib\Core\Registry();
+	$registry = new \App\Lib\Core\Registry();
 
 	// Loader
-	$registry->oLoader = $loader;  	
+	$registry->oLoader = $loader;
 
  	// Session
- 	$oSession = new \App\Lib\Session();
- 	$registry->oSession = $oSession;
+	$oSession = new \App\Lib\Session();
+	$registry->oSession = $oSession;
 
  	// Input
- 	$oInput = new \App\Lib\Input();
- 	$registry->oInput = $oInput; 	 	
- 	
+	$oInput = new \App\Lib\Input();
+	$registry->oInput = $oInput;
+
 	// Response
 	$response = new \App\Lib\Core\Response();
 	$response->addHeader('Content-Type: text/html; charset=utf-8');
-	$registry->oResponse = $response; 
+	$registry->oResponse = $response;
 
 	// Config
-	$registry->oConfig = $config; 
+	$registry->oConfig = $config;
 	
 	// Parameter
 	$view = new \App\Lib\Core\View();
@@ -136,4 +136,4 @@
 	{
 		$oBenchmark->mark('code_end');
 		echo "<br>".$oBenchmark->elapsed_time('code_start', 'code_end');
-	}	
+	}
